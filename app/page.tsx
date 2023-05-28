0478c86b056f5b59b0bec0112dfefb07d1c12346
robinver <robin.vermote@telenet.be>
2023-05-28 19:51:04 +0900

    firstCommit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,177 @@
-import Image from 'next/image'
+"use client"
+import {useEffect, useState} from "react"
+import axios from "axios"
 
 export default function Home() {
+
+  type Pokemon=any
+  type data=any
+  type PokemonData={
+    jsonData: {[key:string]: any},
+    headers: {[key:string]: any},
+    pokemonArray: any[]
+  }
+  const [data,setData] = useState<PokemonData>({jsonData: {}, headers:[],pokemonArray:[]})
+  const [curPokemon, setCurPokemon] = useState<number>(0)
+  const [initPokemonArray, setInitPokemonArray] = useState<any[]>([])
+
+  enum tableOrder{
+    Unsorted,
+    Ascending,
+    Descending,
+  }
+
+  const [tableSorted, setTableSorted] = useState<{[key:string]: any}>({})
+  const [inputFieldValue, setInputFieldValue] = useState<string>("")
+
+  useEffect(() => {
+    axios.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json")
+      .then(res => {
+        const arr = dataToArray(res.data)
+        setData({jsonData:res.data, headers:columns, pokemonArray:arr})
+        setInitPokemonArray([...arr])
+      })
+  },[])
+
+  useEffect(() => {
+    let newTable: {[key:string]: any} = {}
+    Object.keys(columns).forEach(key => newTable[key]=tableOrder.Unsorted)
+    setTableSorted(newTable)
+  },[])
+
+  const columns = {
+    sprite: "Sprite",
+    name: "Name",
+    type1: "Type 1",
+    type2: "Type 2",
+    HP: "HP",
+    attack: "Attack",
+    defense: "Defense",
+    spAttack: "Sp. Attack",
+    spDefense: "Sp. Defense",
+    speed: "Speed",}
+
+  const dataToArray = (input:any):any[] => {
+    let res:any[]=[]
+
+    for(let {base,name,type,id,...rest} of input){
+      res.push({
+        id: id,
+        HP: base.HP,
+        attack: base.Attack,
+        defense: base.Defense,
+        spAttack: base["Sp. Attack"],
+        spDefense: base["Sp. Defense"],
+        speed: base["Speed"],
+        name: name.english,
+        type1: type["0"],
+        type2: type["1"]
+      })
+    }
+
+    return res
+  }
+
+  const changeOrder = (key:string) => {
+    if (tableSorted[key]===tableOrder.Descending){
+      tableSorted[key]=tableOrder.Unsorted
+    }
+    else if (tableSorted[key]===tableOrder.Ascending){
+      tableSorted[key]=tableOrder.Descending
+    }
+    else if (tableSorted[key]===tableOrder.Unsorted){
+      tableSorted[key]=tableOrder.Ascending
+    }
+  }
+
+  const sortColumn = (key:string) => {
+    
+    if (tableSorted[key]===tableOrder.Unsorted){
+      setData({...data,pokemonArray:[...initPokemonArray]})
+      return
+    }
+
+    const newData = {...data}
+    newData.pokemonArray.sort((a:any, b:any) => {
+      const valA = a[key]
+      const valB = b[key]
+
+      if (tableSorted[key]===tableOrder.Ascending){
+        if (valA < valB) return -1
+        if (valB > valA) return 1
+        return 0
+      }else{
+        if (valA > valB) return -1
+        if (valB < valA) return 1
+        return 0
+      }
+    })
+    setData(newData)
+  }
+
+  const filterRows = (rows:Object[]):any[] => {
+    return rows.filter(row => Object.values(row).some(ele => String(ele).toLowerCase().includes(inputFieldValue)))
+  }
+
+  const scrollToTop = () =>{
+    window.scrollTo({top:0, behavior:"smooth"})
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <body>
+    <header className="sticky top-0 bg-[#212121] p-6 border-b border-gray-600 text-white font-bold">
+      <h1 className="hover:cursor-pointer left-0 inline-block whitespace-nowrap" onClick={scrollToTop}>POKE LOOKUP</h1>
+    </header>
+    <main className="font-bold bg-[#212121] text-white min-h-screen">
+      <div className="flex flex-col justify-items-center max-w-[600px] mx-auto py-10">
+
+        <div className="mx-auto">
+          <select id="PokemonSelect" onChange={(e) => setCurPokemon((document.getElementById("PokemonSelect") as HTMLSelectElement)?.selectedIndex)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#3d3d3d] dark:border-gray-400 dark:placeholder-gray-300 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {Object.keys(data.jsonData).map((key,pokemonidx) => <option key={pokemonidx}> {data.jsonData[key].name.english} </option>)}
+          </select>
+
+          <div className="flex flex-col items-center my-5">
+            <div>{data.jsonData[curPokemon]?.name?.english}</div>
+            <div><img src={"https://img.pokemondb.net/sprites/home/normal/"+(data.jsonData[curPokemon]?.name?.english)?.toLowerCase()+".png"}></img></div>
+          </div>
+
+          <div className="flex justify-between border rounded-lg m-4">
+            <button className="grow hover:bg-[#3d3d3d]" onClick={() => {
+              let newVal = curPokemon-1
+              if (curPokemon===0){
+                newVal=Object.keys(data.jsonData).length-1}
+              setCurPokemon(newVal)
+            }}>&lt;</button>
+            <button className="grow hover:bg-[#3d3d3d]" onClick={() => {
+              curPokemon===Object.keys(data.jsonData).length-1 ? setCurPokemon(0) : setCurPokemon(curPokemon+1)
+            }}>&gt;</button>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <input type="text" placeholder="Search" onChange={(e) => setInputFieldValue(e.target.value)} className="rounded-lg border-gray-300 bg-[#3d3d3d] px-2 py-0.5 m-2"></input>
+          <table>
+            <thead>
+              <tr>
+                {Object.keys(data.headers).map((key:string) => <th className="bg-[#3d3d3d] hover:cursor-pointer px-3" id={key} onClick={_ => {changeOrder(key); sortColumn(key)}}> {data.headers[key]} </th>)}
+              </tr>
+            </thead>
+            <tbody>
+              {filterRows(data.pokemonArray).map(row => 
+                <tr> {Object.keys(data.headers).map(key => {
+                    if (key==="sprite")
+                      return <td className='bg-[#3d3d3d]'><img src={"https://img.pokemondb.net/sprites/sword-shield/icon/"+String(row["name"].toLowerCase()).padStart(3,"0")+".png"}></img></td>
+                    else if (key==="name")
+                      return <td className='bg-[#3d3d3d] py-2' onClick={_ => {setCurPokemon(row["id"]-1), scrollToTop()}}> <span className="hover:underline hover:cursor-pointer">{row[key]}</span> </td>
+                    else
+                      return <td className='bg-[#3d3d3d] py-2'> {row[key]} </td>
+                    })} </tr>)}
+            </tbody>
+          </table>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
+    </body>
   )
 }
